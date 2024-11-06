c5c159e8ffa805035cbff26c9355794b3fa52876
yury-lysogorskiy <yury.lysogorskiy@aceworks.works>
2024-11-06 16:13:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tensorpotential",
-    version="0.4.0",
+    version="0.4.1",
     packages=find_packages(include=["tensorpotential", "tensorpotential.*"]),
     url="https://github.com/ICAMS/grace-tensorpotential",
     license="Academic Software License (ASL)",
